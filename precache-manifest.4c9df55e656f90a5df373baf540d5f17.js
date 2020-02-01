d2eeb3101df0564cff4ef9f7dbff79cca9b12bb1
mona mohamed <monaelhoby@yahoo.com>
2020-02-01 17:42:11 +0200

    Updates

---
+++ precache-manifest.4c9df55e656f90a5df373baf540d5f17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2079287ac853e3e65774e57ed7c0b8",
+    "url": "/App/index.html"
+  },
+  {
+    "revision": "4d75fd8a6407869b9ac4",
+    "url": "/App/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "f90b410471aa1d1438d4",
+    "url": "/App/static/css/main.d9bb222f.chunk.css"
+  },
+  {
+    "revision": "4d75fd8a6407869b9ac4",
+    "url": "/App/static/js/2.c5bea3ac.chunk.js"
+  },
+  {
+    "revision": "f90b410471aa1d1438d4",
+    "url": "/App/static/js/main.8ad99e98.chunk.js"
+  },
+  {
+    "revision": "eaf2a35e258a5af3ac5e",
+    "url": "/App/static/js/runtime-main.8d18bf02.js"
+  },
+  {
+    "revision": "c9708885f25a926425702d13f3f75e35",
+    "url": "/App/static/media/Map.c9708885.png"
+  }
+]);
